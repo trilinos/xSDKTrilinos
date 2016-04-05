adc1ccb0787d5b7600610f69890ab27437dcbc03
Barry Smith <bsmith@mcs.anl.gov>
2016-04-04 22:35:19 -0500

    Update for new KSPMonitorDefault() usage
    Funded-by: IDEAS
    Project: xSDK
    Time: .1 hour
    Reported-by: Alicia Marie Klinvex <amklinv@sandia.gov>

--- petsc/src/BelosPETScSolMgr.hpp
+++ petsc/src/BelosPETScSolMgr.hpp
@@ -622,7 +622,10 @@ ReturnType PETScSolMgr<ScalarType,MV,OP>::solve()
 
   // Tell the solver whether to output convergence information
   if(verbosity_ & IterationDetails || verbosity_ & StatusTestDetails) {
-    ierr = KSPMonitorSet(solver, KSPMonitorDefault, NULL, NULL); CHKERRCONTINUE(ierr);
+    PetscViewerAndFormat *vf;
+    ierr = PetscViewerAndFormatCreate(PETSC_VIEWER_STDOUT_(PetscObjectComm((PetscObject)solve)),PETSC_VIEWER_DEFAULT);CHKERRQ(ierr);
+    ierr = PetscObjectDereference((PetscObject)vf->viewer);CHKERRQ(ierr);
+    ierr = KSPMonitorSet(solver, (PetscErrorCode (*)(KSP,PetscInt,PetscReal,void*))KSPMonitorDefault, vf, (PetscErrorCode (*)(void**))PetscViewerAndFormatDestroy); CHKERRCONTINUE(ierr);
   }
 
   // Wrap the Trilinos Operator in a PETSc Mat

4028626f6039be12322492be6edb8026d4f71a05
Alicia Klinvex <amklinv@sandia.gov>
2016-04-07 12:03:42 -0600

    xSDKTrilinos: Improved the PETSc->MueLu example
    This example now returns an error code if the residual is too large.

--- petsc/example/PETSc_MueLuEx.cpp
+++ petsc/example/PETSc_MueLuEx.cpp
@@ -87,16 +87,18 @@ PetscErrorCode ShellApplyML(PC pc,Vec x,Vec y);
 
 int main(int argc,char **args)
 {
-  Vec            x,b;            /* approx solution, RHS  */
-  Mat            A;              /* linear system matrix */
-  KSP            ksp;            /* linear solver context */
-  PC pc;
-  PetscRandom    rctx;           /* random number generator context */
-  PetscInt       i,j,Ii,J,Istart,Iend;
-  PetscInt       m = 50,n = 50;  /* #mesh points in x & y directions, resp. */
-  PetscErrorCode ierr;
-  PetscScalar    v;
-  MPI_Comm comm;
+  Vec                   x,b;            /* approx solution, RHS  */
+  Mat                   A;              /* linear system matrix */
+  KSP                   ksp;            /* linear solver context */
+  PC                    pc;
+  PetscRandom           rctx;           /* random number generator context */
+  PetscInt              i,j,Ii,J,Istart,Iend;
+  PetscInt              m = 50,n = 50;  /* #mesh points in x & y directions, resp. */
+  PetscErrorCode        ierr;
+  PetscScalar           v;
+  PetscInt              rank=0;
+  MPI_Comm              comm;
+  PetscViewerAndFormat* vf;
 
   //
   // Initialize PETSc
@@ -112,6 +114,7 @@ int main(int argc,char **args)
   ierr = MatMPIAIJSetPreallocation(A,5,PETSC_NULL,5,PETSC_NULL);CHKERRQ(ierr);
   ierr = MatSetUp(A);CHKERRQ(ierr);
   ierr = PetscObjectGetComm( (PetscObject)A, &comm);CHKERRQ(ierr);
+  ierr = MPI_Comm_rank(comm,&rank);CHKERRQ(ierr);
   ierr = MatGetOwnershipRange(A,&Istart,&Iend);CHKERRQ(ierr);
 
   for (Ii=Istart; Ii<Iend; Ii++) { 
@@ -140,82 +143,135 @@ int main(int argc,char **args)
   ierr = MatMult(A,x,b);CHKERRQ(ierr);
 
   //
-  // Create the initial guess
-  //
-  ierr = VecSet(x,0);CHKERRQ(ierr);
-
-  //
   // Copy the PETSc matrix to a Tpetra CrsMatrix
   //
   RCP<CrsMatrix> tpetraA = xSDKTrilinos::deepCopyPETScAIJMatrixToTpetraCrsMatrix<Scalar,LO,GO,Node>(A);
 
-  /* Copy the PETSc vectors to Tpetra vectors. */
+  //
+  // Copy the PETSc vectors to Tpetra vectors.
+  //
   RCP<Vector> tpetraX = xSDKTrilinos::deepCopyPETScVecToTpetraVector<Scalar,LO,GO,Node>(x);  
   RCP<Vector> tpetraB = xSDKTrilinos::deepCopyPETScVecToTpetraVector<Scalar,LO,GO,Node>(b);
 
-  /* Create the MueLu AMG preconditioner. */
+  //
+  // Set initial guess to 0
+  //
+  tpetraX->putScalar(0);
 
-  /* Parameter list that holds options for AMG preconditioner. */
+  //
+  // Create the MueLu AMG preconditioner.
+  //
+
+  // Parameter list that holds options for AMG preconditioner. 
   Teuchos::ParameterList mlList;
   mlList.set("parameterlist: syntax", "ml");
-  /* Specify how much information ML prints to screen.
-     0 is the minimum (no output), 10 is the maximum. */
+  // Specify how much information ML prints to screen.
+  // 0 is the minimum (no output), 10 is the maximum.
   mlList.set("ML output",0);
   mlList.set("smoother: type (level 0)","symmetric Gauss-Seidel");
 
-  /* how many fine grid pre- or post-smoothing sweeps to do */
+  // how many fine grid pre- or post-smoothing sweeps to do
   mlList.set("smoother: sweeps (level 0)",1);
 
   RCP<MueLuOp> Prec = MueLu::CreateTpetraPreconditioner(Teuchos::rcp_dynamic_cast<OP>(tpetraA), mlList);
 
-  /* Trilinos CG */
-  RCP<LP> Problem = rcp(new LP(tpetraA, tpetraX, tpetraB));
-  Problem->setLeftPrec(Prec);
-  Problem->setProblem();
+  //
+  // Create parameter list for the Belos solver manager
+  //
   RCP<Teuchos::ParameterList> cgPL = rcp(new Teuchos::ParameterList());
   cgPL->set("Maximum Iterations", 200);
   cgPL->set("Verbosity", Belos::StatusTestDetails + Belos::FinalSummary);
   cgPL->set("Convergence Tolerance", 1e-8);
+
+  //
+  // Construct a preconditioned linear problem 
+  //
+  RCP<LP> Problem = rcp(new LP(tpetraA, tpetraX, tpetraB));
+  Problem->setLeftPrec(Prec);
+  Problem->setProblem();
+
+  //
+  // Create a Belos iterative solver manager and perform the linear solve
+  //
   SolMgr solver(Problem,cgPL);
   solver.solve();
 
-  /* PETSc CG */
+  //
+  // Check the residual
+  //
+  Vector R( tpetraA->getRowMap() );
+  tpetraA->apply(*tpetraX,R);
+  R.update(1,*tpetraB,-1);
+  std::vector<double> normR(1), normB(1);
+  R.norm2(normR);
+  tpetraB->norm2(normB);
+  if(rank == 0) std::cout << "Belos relative residual: " << normR[0] / normB[0] << std::endl;
+  if(normR[0] / normB[0] > 1e-8)
+    return EXIT_FAILURE;
+  
+  //
+  // Check the error
+  //
+  RCP<Vector> trueX = xSDKTrilinos::deepCopyPETScVecToTpetraVector<Scalar,LO,GO,Node>(x);
+  Vector errorVec( tpetraA->getRowMap() );
+  errorVec.update(1,*tpetraX,-1,*trueX,0);
+  std::vector<double> normErrorVec(1);
+  errorVec.norm2(normErrorVec);
+  if(rank == 0) std::cout << "Belos error: " << normErrorVec[0] << std::endl;
+  if(normErrorVec[0] > 1e-6)
+    return EXIT_FAILURE;
+
+  //
+  // Create a PETSc KSP linear solver
+  //
   ierr = KSPCreate(PETSC_COMM_WORLD,&ksp);CHKERRQ(ierr);
   ierr = KSPSetOperators(ksp,A,A);CHKERRQ(ierr);
   ierr = KSPSetTolerances(ksp,1e-8,1.e-50,PETSC_DEFAULT,
                           PETSC_DEFAULT);CHKERRQ(ierr);
   ierr = KSPSetType(ksp,KSPCG);CHKERRQ(ierr);
 
-  /* Wrap the ML preconditioner as a PETSc shell preconditioner. */
+  //
+  // Wrap the ML preconditioner as a PETSc shell preconditioner.
+  //
   ierr = KSPGetPC(ksp,&pc);CHKERRQ(ierr);
   ierr = PCSetType(pc,PCSHELL);CHKERRQ(ierr);
   ierr = PCShellSetApply(pc,ShellApplyML);CHKERRQ(ierr);
   ierr = PCShellSetContext(pc,(void*)Prec.get());CHKERRQ(ierr);
   ierr = PCShellSetName(pc,"MueLu AMG");CHKERRQ(ierr); 
 
-  /* Solve the linear system using PETSc */
+  //
+  // Solve the linear system using PETSc 
+  //
   ierr = KSPSetFromOptions(ksp);CHKERRQ(ierr);
+  ierr = PetscViewerAndFormatCreate(PETSC_VIEWER_STDOUT_WORLD,PETSC_VIEWER_DEFAULT,&vf);CHKERRCONTINUE(ierr);
+  ierr = KSPMonitorSet(ksp, (PetscErrorCode (*)(KSP,PetscInt,PetscReal,void*))KSPMonitorDefault, vf, (PetscErrorCode (*)(void**))PetscViewerAndFormatDestroy);CHKERRCONTINUE(ierr);
   ierr = KSPSolve(ksp,b,x);CHKERRQ(ierr);
 
-  int its;
-  PetscReal rnorm;
-  ierr = KSPGetIterationNumber(ksp,&its);CHKERRQ(ierr);
-  ierr = KSPGetResidualNorm(ksp,&rnorm);CHKERRQ(ierr);
-  std::cout << "Number of KSP iterations: " << its << std::endl;
-  std::cout << "KSP residual norm: " << rnorm << std::endl;
+  //
+  // Check the residual
+  //
+  RCP<Vector> kspX = xSDKTrilinos::deepCopyPETScVecToTpetraVector<Scalar,LO,GO,Node>(x);
+  tpetraA->apply(*kspX,R);
+  R.update(1,*tpetraB,-1);
+  R.norm2(normR);
+  tpetraB->norm2(normB);
+  if(rank == 0) std::cout << "KSP relative residual: " << normR[0] / normB[0] << std::endl;
+  if(normR[0] / normB[0] > 1e-8)
+    return EXIT_FAILURE;
 
-  /* Print the obtained solutions */
-//  ierr = VecView(x,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
-//  Teuchos::FancyOStream fancyStream(Teuchos::rcpFromRef(std::cout));
-//  tpetraX->describe(fancyStream,Teuchos::VERB_EXTREME);
-
+  //
+  // Free PETSc memory
+  //
   ierr = KSPDestroy(&ksp);CHKERRQ(ierr);
   ierr = VecDestroy(&x);CHKERRQ(ierr);
   ierr = VecDestroy(&b);CHKERRQ(ierr);  
   ierr = MatDestroy(&A);CHKERRQ(ierr);
 
+  //
+  // Terminate PETSc
+  //
   ierr = PetscFinalize();CHKERRQ(ierr);
-  return 0;
+  return EXIT_SUCCESS;
 } /*main*/
 
 /* ***************************************************************** */
